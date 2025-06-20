caf09cb8c5ab53c180cafefeb3f6a3d3d530e8c6
UpulnPerera <upulnisantha69@gmail.com>
2025-06-19 21:21:57 -0700

    first commit

---
+++ 5PyPrg.py
@@ -0,0 +1,10 @@
+# Value Addition
+a = int(input("Please enter your first number: "))
+b = int(input("Please enter your first number: "))
+c = str(input("Please enter your + for addition or - for difference: "))
+
+
+sum = a + b
+print(f"total sum is {sum}")
+sum = a - b
+print(f"total difference is {sum}")
